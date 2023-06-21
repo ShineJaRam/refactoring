6f5c8edd771a32751a2887d97c9c756f2180e982
SuhyunLim <suhuyn19@naver.com>
2023-06-22 01:21:27 +0900

    tc 작성중

---
+++ chapter4/test/shortage.test.js
@@ -0,0 +1,8 @@
+import Province from "../Province";
+import sampleProvinceData from "../sampleProvinceData";
+
+describe("province", function () {
+  it("shortfall", function () {
+    const asia = new Province(sampleProvinceData());
+  });
+});
